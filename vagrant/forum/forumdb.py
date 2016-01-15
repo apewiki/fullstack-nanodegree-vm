df9bb1fbad3db7787d653ed68b7774cfed104493
apewiki <zhu1996@yahoo.com>
2016-01-15 15:56:23 -0500

    examples

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,13 @@
 #
 # Database access functions for the web forum.
-# 
-
+#
+import psycopg2
 import time
+import bleach
 
 ## Database connection
-DB = []
+#db = sqlite3.connect("forum")
+DB=[]
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +18,16 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    db = psycopg2.connect("dbname=forum")
+    if db:
+      c = db.cursor()
+      query = "select content, time from posts;"
+      print query
+      c.execute(query)
+      DB = c.fetchall()
+      posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+      posts.sort(key=lambda row: row['time'], reverse=True)
+      db.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +37,15 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    new_content = bleach.clean(content)
+    print new_content
+
+    db = psycopg2.connect("dbname = forum")
+    c = db.cursor()
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    #DB.append((t, content))
+    # use tuple for query?? query is performed safely this way, python safe mode
+    #query paramter instead of string substitution
+    c.execute("insert into posts (content) values (%s) ", (new_content,))
+    db.commit()
+    db.close()
